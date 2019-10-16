2ec21c90536b4bbf8eaa33891d51ff6e15783dae
Andrew Nelson <andy@INFINITYROCK.INFINITYELS.COM>
2019-10-16 10:25:47 -0700

    Minor layout adjustments. Added Deck object to game.js. Card back and card faces now auto generate.

--- js/game.js
+++ js/game.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-    
+   
+    const Deck = {
+        cardCount: 16, //replace with call to method from Game
+        imageCount: 51,
+        cardBack() {
+            const image = Math.floor(Math.random() * this.imageCount);
+            $('.card-back').attr('src', 'images/' + image + '.png');
+        },
+        buildDeck() {
+            const face = [];
+            for(var i = 0; i < this.imageCount; i++) {
+                face[i] = 'images/' + (i + 1) + '.png';
+            }
+            $('.card-face').attr('src', face[38]);
+        }
+
+    };
+    Deck.cardBack();
+    Deck.buildDeck();
+   
+    // i = 42;
+    //$('.test').attr('src', 'images/' + i + '.png');
 });
 
 //load images half the images needed, duplicate them once, shuffle images, add them to card-face.
