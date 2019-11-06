4ee9f1d5dc65d0aca840eda2d167c04500f300ae
Andy <andy.nelson.054@gmail.com>
2019-11-05 22:18:13 -0800

    Minor changes to game.js

--- js/game.js
+++ js/game.js
@@ -3,62 +3,48 @@ $(document).ready(function() {
     const Deck = {
         gameCards: 16 / 2, //replace with call to method from Game
         totalCards: 16,
-        imageCount: 51,
+        imageCount: 50,
         get cardBack() {
-           const cardBackSelect = Math.floor(Math.random() * (this.imageCount + 1));
+           const cardBackSelect = Math.floor(Math.random() * (this.imageCount) + 1);
            return 'images/' + cardBackSelect + '.png';
         },
         get cardFaces() {
-            var cardFaces = [];
-            var cardFaceNum = new Array(this.totalCards);
-            var lowerHalfRand = Math.floor((Math.random() * this.gameCards));
+            var cardFace = new Array(this.totalCards);
+            var faceMax = 2;
+            var faceCount = 0;
+            var randCard;
+
             //generate random non-repeating values for first half of array
             for(var i = 0; i < this.gameCards; i++){
-                
                 randCard = Math.floor(Math.random() * this.imageCount + 1);
-                cardFaceNum[i] = randCard;
-                cardFaceNum[i + this.gameCards] = randCard;
-                
+                cardFace[i] = randCard;
                 for(var j = 0; j < i; j++) { //reassign repeated card faces
-                    if(cardFaceNum[j] == randCard) {  //if newest card matches a previous one 
-                        randCard = Math.floor(Math.random() * this.imageCount); //reset new card so it doesn't match 
-                        cardFaceNum[i] = randCard;
-                        cardFaceNum[i + this.gameCards] = randCard; //duplicate value in secod half of array
-                        j = -1; //reset counter to check new value again
+                    if(cardFace[j] === randCard) {  //if newest card matches a previous one 
+                        faceCount++;
+                        if(faceCount === faceMax){//reset counter to check new value again
+                        }
                     } 
-                } 
+                }
             }
 
-            /* instead
-            console.log(lowerHalfRand);
-            //dublicate values once each for second half of array
-            for(var i = this.gameCards; i < this.totalCards; i++){
-                
-                cardFaceNum[i] = cardFaceNum[Math.floor((Math.random() * this.gameCards))];
-                
-                for(var j = this.gameCards; j < i; j++) { //reassign repeated card faces
-                    if(cardFaceNum[j + this.gameCards] == cardFaceNum[j]) {  //if newest card matches a previous one 
-                        cardFaceNum[j] = cardFaceNum[Math.floor((Math.random() * this.gameCards))]; //reset new card so it doesn't match 
-                        j = this.gameCards - 1; //reset counter to check new value again
-                    } 
-                } 
+            //assign upper half of cardFace
+            var upperHalf = new Array(this.gameCards);
+            var randLowerIndex = Math.floor(Math.random() * this.gameCards);
+            for(var i = 0; i < this.gameCards; i++){
+
+            }
+
+
+            /*for(var i = 0; i < cardFace.length; i++) {
+                cardFaces[i] = 'images/' + cardFace[i] + '.png';
             }*/
-
-            
-
-
-            /*for(var i = 0; i < cardFaceNum.length; i++) {
-                cardFaces[i] = 'images/' + cardFaceNum[i] + '.png';
-            }*/
-            console.log(cardFaceNum);
-            return cardFaceNum;
-        },
-        shuffle(){
-
+            console.log(cardFace);
+            return cardFace;
         },
         buildDeck() {
             $('.card-back').attr('src', this.cardBack);
-            $('.card-face').attr('src', 'images/' + this.cardFaces[Math.floor(Math.random() * this.imageCount)] + '.png');
+            $('.card-face').attr('src', 'images/' + this.cardFaces[Math.floor(Math.random() * this.imageCount) + 1] + '.png');
+
         }
             /*const deck = [];
             const imageSelect = Math.floor(Math.random() * this.imageCount);
@@ -74,7 +60,7 @@ $(document).ready(function() {
         }*/
         
     };
-    console.log('cardFaceNum: ' + Deck.cardFaces);
+    console.log('cardFace: ' + Deck.cardFaces);
     
     Deck.buildDeck();
     console.log(Deck);
@@ -82,5 +68,3 @@ $(document).ready(function() {
     // i = 42;
     //$('.test').attr('src', 'images/' + i + '.png');
 });
-
-//load images half the images needed, duplicate them once, shuffle images, add them to card-face.
