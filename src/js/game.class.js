f37c315a03904457efe20863129db349ead29939
Andy Nelson <andy.nelson.054@gmail.com>
2020-06-03 11:13:54 -0700

    Moved a bunch of stuff from main.js into init method in game.class.js. Added reveal method in game.class.js which doesn't work with cardFlip method yet

--- src/js/game.class.js
+++ src/js/game.class.js
@@ -14,6 +14,64 @@ export class Game {
         this.cards = document.getElementsByClassName('card-title');
         this.score = 0;
     }
+
+    init() {
+        const gameTimer = new Timer(60);
+        const newDeck = new Deck(8, 16);
+        newDeck.buildBoard();
+        
+        const gameTable = document.getElementById('outer-board');
+        const gameBoard = document.getElementById('inner-board');
+        const coverMenu = document.getElementById('cover-menu');
+        const controlPanel = document.getElementById('control-panel');
+        const timerStop = document.getElementById('timer-stop');
+        
+        //reveal game board
+        $(gameBoard).removeClass('closed');
+        $(coverMenu).attr('hidden', true);
+        $(controlPanel).attr('hidden', false);
+
+        this.cardReveal();
+
+        gameTable.addEventListener('click', () => {
+            // card clicks 
+            if(event.target.classList.contains('click')) {
+                console.log(`event.target: ${event.target}`);
+                if(!gameTimer.isRunning) {
+                    gameTimer.run();
+                }
+                //this.cardClick(event.target);
+            }
+
+            //timer stop
+            if(event.target === timerStop) {
+                gameTimer.stop();
+            }
+
+        }, false);
+    }
+
+    cardReveal() {
+        //Need to better understand why this works
+        let cardBacks = document.getElementsByClassName('game-card'); //remove
+        for (var i = 0; i <= 50; i++) {
+
+            (function(){
+       
+                var j = i;
+                setTimeout(function() { 
+                    cardBacks[j].click(); 
+                    cardBacks[j].classList.add('revealed');
+                }, 25 * j);
+       
+            })();
+       
+       }
+
+       
+    };
+    
+
     /*
     * @desc getter function to randomly selec image from /images directory to be
      displayed on card backs
@@ -21,6 +79,7 @@ export class Game {
     * @return string - filepath to selected image
     */
     cardClick(clickedCard) {
+        console.log(`clickedCard: ${clickedCard}`);
         //clickedCard.classList.indexOf('card-title') ? alert('Double') : alert('Single');
         this.flippedCount++;
         this.cardFlip();
@@ -34,6 +93,7 @@ export class Game {
     * @return string - filepath to selected image
     */
    cardFlip() {
+    console.log('cardFlip');
     if(this.flippedCount === this.flippedMax) {
             //flip cards after half a second
             setTimeout(() => {
